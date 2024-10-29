b40bb8330031fb5e6e883f7334ae2b36fcc9dbfb
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-10-29 15:00:34 +0800

    update workflow

--- config/config.js
+++ config/config.js
@@ -1,17 +1,10 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-    username: process.env.MYSQL_DEV_USERNAME,
-    password: process.env.MYSQL_DEV_PASSWORD,
-    host: process.env.MYSQL_DEV_HOST,
-    database: process.env.MYSQL_DATABASE,
-    dialect: 'mysql'
-  },
-  production: {
-    username: process.env.MYSQL_PROD_USERNAME,
-    password: process.env.MYSQL_PROD_PASSWORD,
-    host: process.env.MYSQL_PROD_HOST,
+  [process.env.NODE_ENV]: {
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
     dialect: 'mysql'
   }
