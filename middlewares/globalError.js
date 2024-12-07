f834ac9dc731cd6a20af9a78c8b42bac7e1cfbc7
alejandrocsdev <alejandrocsdev@gmail.com>
2024-12-07 12:44:22 +0800

    add (eslint-disable-next-line no-unused-vars)

--- middlewares/globalError.js
+++ middlewares/globalError.js
@@ -1,5 +1,6 @@
 // 全域錯誤訊息中間件
-function globalError(err, req, res) {
+// eslint-disable-next-line no-unused-vars
+function globalError(err, req, res, next) {
   console.info(err)
 
   res.status(err.code || 500).json({ message: err.message, i18n: err.i18n || 'error.defaultError' })
